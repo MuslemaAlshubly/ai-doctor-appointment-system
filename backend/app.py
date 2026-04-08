2cc287257374f83cc25fb35ea0b6a263189ab1d1
unknown <60305349@udst.edu.qa>
2026-04-08 13:04:44 +0300

    fix: import of openai

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,10 @@ from flask_cors import CORS
 from backend.db import db
 from datetime import datetime
 from flask_cors import CORS
+import openai
 
 app = Flask(__name__)
 CORS(app)
-
-
 
 
 @app.route("/")
